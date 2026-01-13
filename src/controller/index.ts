7f24abe2a89c1e9676e4219b1416736bf3c238fe
nico-slk <nicolas.selicki@gmail.com>
2026-01-13 12:42:25 -0300

    import file + dashboard info

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,2 +1,3 @@
 export { default as GastoController } from "./gasto.controller";
+export { default as LineChartController } from "./linecharts.controller";
 export { default as VentaController } from "./venta.controller";
